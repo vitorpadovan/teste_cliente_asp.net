3760b105bcfffde284f679173e7f0758fa897b7a
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2023-02-19 00:18:19 -0300

    Adicionando mascara na saída da lista

--- CadastroClienteBff/Controllers/ClienteController.cs
+++ CadastroClienteBff/Controllers/ClienteController.cs
@@ -2,6 +2,7 @@
 using CadastroClienteBff.Business;
 using CadastroClienteBff.Config.Exceptions;
 using CadastroClienteBff.Controllers.Contracts.Request;
+using CadastroClienteBff.Controllers.Contracts.Response;
 using CadastroClienteBff.Model;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,31 +13,45 @@ namespace CadastroClienteBff.Controllers
     public class ClienteController : ControllerBase
     {
         ClienteBusiness c = new ClienteBusiness();
+        private IMapper mapper;
 
-        
+        public ClienteController()
+        {
+            this.mapper = ConfigurarMapper();
+        }
+
         [HttpPost(Name = "SalvarCliente")]
-        public Cliente salvarCliente(SalvarClienteRequest request)
+        public SalvarClienteResponse salvarCliente(SalvarClienteRequest request)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(1, "Chato");
+
+            var cliente = mapper.Map<Cliente>(request);
+            this.c.salvarCliente(cliente);
+            var response = mapper.Map<SalvarClienteResponse>(cliente);
+            return response;
+        }
+
+        [HttpGet(Name = "GetClientes")]
+        public List<SalvarClienteResponse> getListaCliente()
+        {
+            List<SalvarClienteResponse> response = new List<SalvarClienteResponse>();
+            c.getListaClientes().ForEach((x) => response.Add(mapper.Map<SalvarClienteResponse>(x)));
+            return response;
+        }
+
+        private static IMapper ConfigurarMapper()
+        {
             var configuration = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<Cliente, SalvarClienteRequest>();
                 cfg.CreateMap<SalvarClienteRequest, Cliente>();
+                cfg.CreateMap<SalvarClienteResponse, Cliente>();
+                cfg.CreateMap<Cliente, SalvarClienteResponse>();
             });
 
             var mapper = configuration.CreateMapper();
-
-            var c = mapper.Map<Cliente>(request);
-            this.c.salvarCliente(c);
-            return c;
-        }
-
-        [HttpGet(Name = "GetClientes")]
-
-        public List<Cliente> getListaCliente()
-        {
-            return c.getListaClientes();
+            return mapper;
         }
     }
 }
---
+++ CadastroClienteBff/Controllers/Contracts/Response/SalvarClienteResponse.cs
@@ -0,0 +1,28 @@
+﻿using CadastroClienteBff.Config.Extensions;
+using CadastroClienteBff.Model.Enums;
+using Microsoft.Extensions.WebEncoders.Testing;
+
+namespace CadastroClienteBff.Controllers.Contracts.Response;
+public class SalvarClienteResponse
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    private string _CpfCnpj;
+    public string CpfCnpj { get {
+            switch (this.tipoDocumento){
+                case TipoDocumento.Cpf:
+                    return _CpfCnpj.MaskNumber(@"000\.000\.000\-00");
+                case TipoDocumento.CNPJ:
+                    return _CpfCnpj.MaskNumber(@"00\.000\.000\/0000\-00");
+            }
+            return _CpfCnpj; 
+        } set { _CpfCnpj = value; } }
+    public TipoDocumento tipoDocumento { get; set; }
+    public DateOnly dataNascimento { get; set; }
+
+    private string _cep;
+    public string cep { get { return _cep.MaskNumber(@"00000\-000"); } set { _cep = value; } }
+    public string endereco { get; set; }
+    public int numero { get; set; }
+}
